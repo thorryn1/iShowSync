beb4ba2e5b55b365dd332eb7be4f20d50971af36
thorryn1 <thorryn92@gmail.com>
2026-09-18 08:46:20 +0700

    Add fixed iShowSpeed WebM

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-import ishowspeed from "../assets/ishow/ishowspeed.webm";
+export default function() {
 
+    const video = document.createElement("video");
+
+    video.id = "ishowsync-video";
+
+    video.src =
+        "https://raw.githubusercontent.com/thorryn1/iShowSync/main/assets/ishow/iShowSpeed.webm";
+
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    video.playsInline = true;
+    video.preload = "auto";
+
+    video.style.position = "fixed";
+    video.style.right = "387px";
+    video.style.bottom = "2px";
+
+    video.style.width = "95px";
+    video.style.height = "95px";
+
+    video.style.objectFit = "contain";
+    video.style.zIndex = "999999";
+
+    document.body.appendChild(video);
+}
